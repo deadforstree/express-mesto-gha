5cb38877995ecebc622d654ed7f26270988b751f
Peter <deadforstree@gmail.com>
2023-05-25 15:47:37 +0300

    13pr-2it

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const DATA_NOT_FOUND_ERROR_CODE = 404;
+
 const { userRoutes } = require('./user');
 
 const { cardsRoutes } = require('./card');
@@ -9,7 +11,7 @@ const routes = express.Router();
 routes.use('/users', userRoutes);
 routes.use('/cards', cardsRoutes);
 routes.use('/', (req, res) => {
-  res.status(404).send({ message: 'Страница по указанному маршруту не найдена' });
+  res.status(DATA_NOT_FOUND_ERROR_CODE).send({ message: 'Страница по указанному маршруту не найдена' });
 });
 
 exports.routes = routes;
